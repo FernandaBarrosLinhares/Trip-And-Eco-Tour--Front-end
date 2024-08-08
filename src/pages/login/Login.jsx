268b7bf37d471a662406442036adb286c34d371a
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-08-07 22:15:23 -0300

    Auth, template compartilhado, outlet e fix login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,23 @@
-
 import { useForm } from 'react-hook-form'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { useAuth } from '../../context/Auth'
 import  './Login.css'
-
 
 
 function Login(){
 
-
 const {register,handleSubmit, formState}= useForm()
 const { errors, isSubmitting } = formState
-
+const { signIn } = useAuth();
+const navigate = useNavigate();
 
     async function onSubmit(dados) {
       
-        console.log(dados)
-    }
     
-    
-    
+        signIn(dados);
+       
+        navigate('/dashboard');
+    }   
     return(
         
         <>
@@ -26,12 +25,12 @@ const { errors, isSubmitting } = formState
         <main className= "container">
             <div className="formulario">
                 <form onSubmit={handleSubmit(onSubmit)}>
-                 
                 <img 
                         className="mb-4 logo" 
                         src="https://http2.mlstatic.com/D_NQ_NP_813708-MLU70381945405_072023-O.webp" 
                         alt="Trip and eco tour"  
-                        height="150" 
+                        height="150"
+                        width="200" 
                     />
                    
                     <div className="form-floating">
@@ -62,16 +61,16 @@ const { errors, isSubmitting } = formState
                         <label htmlFor="floatingPassword">Password</label>
                     </div>
 
-                    <button className="btn btn-primary w-100 py-2 mb-3" type="submit" disabled={isSubmitting}>{isSubmitting ? 'Carrengado...': 'Entrar'}</button>
-                    <p>
+                    <button className="btn btn-primary w-100 py-2 mb-8" type="submit" disabled={isSubmitting}>{isSubmitting ? 'Carrengado...': 'Entrar'}</button>
+                    
+               
+                    
+                    <p className='cadastro'>
                         Não possui cadastro? 
-                    <Link className='btn-link' to='/cadastroUsuario'>
+                    <Link className='btn-link' to='/login'>
                         <button className='btn btn-outline-primary'>Cadastrar</button>
                     </Link>
                     </p>
-               
-                    
-                    
                 </form>
             </div>
 
