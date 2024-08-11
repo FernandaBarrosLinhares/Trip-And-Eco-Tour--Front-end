ce9a9fa171707ca652fd3d77f00d34c534b7f165
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-08-11 13:47:32 -0300

    Dashboard, Coordenadas Geograficas e alterações em cadastro destino e menu.

--- src/pages/cadastroDestino/CadastroDestino.jsx
+++ src/pages/cadastroDestino/CadastroDestino.jsx
@@ -1,130 +1,106 @@
-import {useForm} from 'react-hook-form'
-// import MenuComponent from "../../components/menu/MenuComponet"
-import EnderecoComponent from '../../components/endereco/Endereco'
-import './CadastroDestino.css'
+import { useForm } from 'react-hook-form';
+import Endereco from '../../components/endereco/Endereco';
+import CoordenadasGeograficas from '../../components/CoordenadaGeografica';
+import './CadastroDestino.css';
 
+function CadastroDestino() {
+   
+    const { register, handleSubmit, setValue, formState } = useForm();
+    const { errors, isSubmitting } = formState;
 
-function CadastroDestino(){
-    const {register,handleSubmit,formState}= useForm()
-    const { errors, isSubmitting } = formState
+   
 
-    async function addDestinys(dados){
+    async function addDestinys(dados) {
         try {
-            const resposta = await fetch('http://localhost:3000/destiny',{
-                method:'POST',
-                body: JSON.stringify(dados)
-              
-            })
+            const resposta = await fetch('http://localhost:3000/destinys', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(dados),
+            });
 
-            if(resposta.ok === false)
-            alert("houve um erro ao cadastrar destino ")
-            
+            if (!resposta.ok) {
+                alert('Houve um erro ao cadastrar destino');
+            }
         } catch (error) {
-            alert("Houve um erro ao cadastrar usuario")
-            
+            alert('Houve um erro ao cadastrar destino');
         }
-        
     }
 
-
-    return(
+    return (
         <>
-        
-            <div >
-                {/* <MenuComponent/> */}
-            </div>
-        
-            <main className='picture-container-destino'>
-            <div className="form-container-destino">
-                <form onSubmit={handleSubmit(addDestinys)}>
-                     <h3 className="titlle mb-3 fw-normal">Preencha todos os campos para adicionar destino!</h3>
+            <main className="picture-container-destino">
+                <div className="form-container-destino">
+                    <form onSubmit={handleSubmit(addDestinys)}>
+                        <h3 className="titlle mb-3 fw-normal">Preencha todos os campos para adicionar destino!</h3>
                         <div className="form-row">
-                            <div className="form-group-destino nome">        
-                            <label htmlFor="nome" className="form-label">Nome do destino</label>
-                            <input
-                                type="text"
-                                id="nome"
-                                className="form-control"
-                                placeholder="Digite o nome do seu destino"
-                                {...register("nome", {
-                                    required: {
-                                        value: true,
-                                        message: "Esse é obrigatório informar nome"
-                                    },
-                                })}
-                            />
-                            {errors.nome && <span className='text-danger text-sm'>{errors.nome.message}</span>}
-                        </div>
-                        <div className="mb-3 form-group-destino descricao">
-                         <label htmlFor="exampleFormControlInput1" className="form-label-destino">Descrição </label>
-                         <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Descreva o local"
-                             {...register("descricao", {
-                             required: {
-                            value: true,
-                            message: "Esse campo é obrigatório"
-                        },
-                    
-                    })}
-                />
-                {errors.descricao && <span className='text-danger text-sm'>{errors.descricao.message}</span>}
-            </div>
-        
-                    </div>
-
-                    <div className="form-row">
-                        <div className="form-group-destino">
-                            <label htmlFor="localizacao" className="form-label">Localização</label>
-                            <input
-                                type="text"
-                                id="localizacao"
-                                className="form-control"
-                                placeholder="Insira a localização"
-                                {...register("localizacao", {
-                                    required: {
-                                        value: true,
-                                        message: "Esse campo é obrigatório"
-                                    },
-                                
-                                })}
-                            />
-                            {errors.localizacao && <span className='text-danger text-sm'>{errors.localizacaomessage}</span>}
+                            <div className="form-group nome">
+                                <label htmlFor="nome" className="form-label">Nome do destino</label>
+                                <input
+                                    type="text"
+                                    id="nome"
+                                    className="form-control"
+                                    placeholder="Digite o nome do seu destino"
+                                    {...register("nome", {
+                                        required: {
+                                            value: true,
+                                            message: "Esse campo é obrigatório",
+                                        },
+                                    })}
+                                />
+                                {errors.nome && <span className="text-danger text-sm">{errors.nome.message}</span>}
+                            </div>
+                            <div className="mb-3 form-group descricao">
+                                <label htmlFor="descricao" className="form-label">Descrição</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Descreva o local"
+                                    {...register("descricao", {
+                                        required: {
+                                            value: true,
+                                            message: "Esse campo é obrigatório",
+                                        },
+                                    })}
+                                />
+                                {errors.descricao && <span className="text-danger text-sm">{errors.descricao.message}</span>}
+                            </div>
                         </div>
 
-                        <EnderecoComponent />
+                        <div className="form-row">
+                            <div className="form-group">
+                                <label htmlFor="localizacao" className="form-label">Localização</label>
+                                <input
+                                    type="text"
+                                    id="localizacao"
+                                    className="form-control"
+                                    placeholder="Insira uma localização"
+                                    {...register("localizacao", {
+                                        required: {
+                                            value: true,
+                                            message: "Esse campo é obrigatório",
+                                        },
+                                    })}
+                                />
+                                {errors.localizacao && <span className="text-danger text-sm">{errors.localizacaomessage}</span>}
+                            </div>
 
+                            <Endereco/>
 
-                        <div className="form-group-destino">
-                            <label htmlFor="coordenadasGeograficas" className="form-label">Coordenadas Geograficas</label>
-                            <input
-                                type="text"
-                                id="coordenadasGeograficas"
-                                className="form-control"
-                                placeholder="00 00 00.0, 0 00 00.0"
-                                
-                            />
-                            
+                            <CoordenadasGeograficas register={register} setValue={setValue} />
                         </div>
-                    </div>
 
-                  
-
-                    <button className="btn btn-success" type="submit" disabled={isSubmitting}>
-                        {isSubmitting ? 'Carregando...' : 'Cadastrar'}
-                    </button>
-                    <button type="button" className="btn btn-secondary">Atualizar</button>
-                    <button type="button" className="btn btn-danger">Deletar</button>
-                </form>
-
-            </div>
-
-        </main>
-        
-
+                        <button className="btn btn-success" type="submit" disabled={isSubmitting}>
+                            {isSubmitting ? 'Carregando...' : 'Cadastrar'}
+                        </button>
+                        <button type="button" className="btn btn-secondary">Atualizar</button>
+                        <button type="button" className="btn btn-danger">Deletar</button>
+                    </form>
+                </div>
+            </main>
         </>
-    )
+    );
 }
 
-export default CadastroDestino
+export default CadastroDestino;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,15 +1,98 @@
-import Menu from '../../components/menu/Menu'
-import {useAuth} from '../../context/Auth'
+import MenuComponent from '../../components/menu/Menu'
 
-function Dashboard(){
-    const dados = useAuth()
-    console.log(dados.signIn)
+import { useAuth } from '../../context/Auth'
+import { useEffect , useState} from 'react'
+import './Dashboard.css'
 
-    return(
+
+function DashBoard(){
+
+const dados = useAuth()
+console.log(dados.signIn)
+
+const [locais,setLocais]=useState([])
+
+
+
+
+useEffect(()=>{
+
+   async function  buscarLocais(){
+    try {
+        const response = await fetch('http://localhost:3000/destiny');
+        if (response.ok) {
+            const data = await response.json();
+            setLocais(data);
+        } else {
+            console.error('Erro ao buscar locais');
+        }
+    } catch (error) {
+        console.error('Erro ao buscar locais', error);
+    }
+   }
+   buscarLocais();
+    }, []);
+
+
+
+
+    return( 
         <>
-        <Menu/>
+        <div className='container-dashboard'>
+            <div className= 'container-menu'>
+                <MenuComponent/>
+            </div>
+            <main className='container-main'>
+                <div className='container-cards'>
+                    <div className="card-user">
+                            <span>Usuários</span>
+                            <span>1</span>
+                            <img src='' />
+                    </div>
+                    <div className="card-destiny">
+                            <span>Destinos</span>
+                            <span>1</span>
+                            <img src='' />
+                    </div>
+                
+          </div>
+          <div className='containerTableandMap'>
+            <div className='header-container'>
+                
+           
+            </div>
+            <table>
+                            <thead>
+                                <tr>
+                                    <th>Local</th>
+                                    <th>Latitude</th>
+                                    <th>Longitude</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {locais.map((local, index) => (
+                                    <tr key={index}>
+                                        <th>{local.nome}</th>
+                                        <th>{local.latitude}</th>
+                                        <th>{local.longitude}</th>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="map-container">
+                            {/* mapa */}
+                        </div>
+            </main>
+
+        </div> 
+         
+
+         
+        
+
         </>
     )
 }
 
-export default Dashboard
+export default DashBoard
